119eb22fddb846cb95e32f1af517d26193c1ab52
Yuriy Stoked <stokedover9k@gmail.com>
2013-06-16 22:47:30 -0400

    better test script which uses diff

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 for file in inout/input-*
 do
   echo "======== ${file} ========"
-  ./linker $file
+  basefile=`basename ${file}`
+  outfile=${basefile/#input/out}
+  ./linker ${file} > out/${outfile}
+  diff -b -B -E out/${outfile} inout/${outfile}
 done
